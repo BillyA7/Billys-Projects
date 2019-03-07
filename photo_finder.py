e83d3da40d9bdc0909d5df75ec066c5cc3282e54
Billy Awan <billy.awan7@gmail.com>
2019-03-07 15:51:33 +0000

    Updated Projects

--- photo_finder.py
+++ photo_finder.py
@@ -14,7 +14,7 @@ count = 0
 # walks through all folders, subfolders and files using itertools.chain to search multiple directorys with os.walk
 for root, subdirs, files in chain.from_iterable(os.walk(directory) for directory in directorys):
     for file in files:
-        # needed to ignore useless photos
+        # ignore useless photos
         if 'joey' not in os.path.join(root, file).lower() and 'e-bay' not in os.path.join(root, file).lower():
             # if the file extension is in the tuple, joins it with the root and copies it to the destination
             if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg', '.bmp', '.png', '.pdd'):
